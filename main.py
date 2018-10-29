f0e4d0d5f5c48d4d6de85c7945561a9353cf5709
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2018-10-29 22:27:13 +0530

    Successfully created a simple database application.

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import sys
 from Qt import QtCore, QtWidgets, QtCompat
-
+import DB_manager
 
 class App(QtWidgets.QDialog):
     """docstring for Life3Coding"""
-    def __init__(self):
+    def __init__(self, database, tableName):
         super().__init__()
-        QtCompat.loadUi('app2.ui', self)
+        QtCompat.loadUi('simple_db_form.ui', self)
         self.setWindowTitle('APP Pyqt Gui')
-        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+        self.dbu = DB_manager.DatabaseUtility(database=database, tableName=tableName)
+        self.pushButton.clicked.connect(self.Commit)
+        self.UpdateTree()
 
-    def on_pushButton_clicked(self):
-        self.label_2.setText('Welcome: ' + self.lineEdit.text())
+    def Commit(self):
+        text = self.lineEdit.text()
+        self.dbu.AddEntryToTable(text)
+        self.UpdateTree()
+
+    def UpdateTree(self):
+        col = self.dbu.GetColumns()
+        table = self.dbu.GetTable()
+
+        for c in range(len(col)):
+            self.treeWidget.headerItem().setText(c, col[c][0])
+
+        self.treeWidget.clear()
+
+        for item in range(len(table)):
+            QtWidgets.QTreeWidgetItem(self.treeWidget)
+            for value in range(len(table[item])):
+                self.treeWidget.topLevelItem(item).setText(value, str(table[item][value]))
 
 
-app = QtWidgets.QApplication(sys.argv)
-widget = App()
-widget.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+
+    db = 'myFirstDB'
+    tableName = 'test8'
+    app = QtWidgets.QApplication(sys.argv)
+    widget = App(db, tableName)
+    widget.show()
+    sys.exit(app.exec_())
