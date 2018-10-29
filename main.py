90557a40b7cf539ae784e21acb93e0614497b6e8
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2018-10-29 21:01:30 +0530

    Created a simple form.

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from Qt import QtCore, QtWidgets, QtCompat
+
+
+class App(QtWidgets.QDialog):
+    """docstring for Life3Coding"""
+    def __init__(self):
+        super().__init__()
+        QtCompat.loadUi('app.ui', self)
+        self.setWindowTitle('APP Pyqt Gui')
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+
+    def on_pushButton_clicked(self):
+        self.label_2.setText('Welcome: ' + self.lineEdit.text())
+
+
+app = QtWidgets.QApplication(sys.argv)
+widget = App()
+widget.show()
+sys.exit(app.exec_())
